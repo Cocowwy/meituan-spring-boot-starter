e786247fa30f1e936512049b004c7eade392615c
Cocowwy <514658459@qq.com>
2022-04-13 21:00:14 +0800

    test

---
+++ meituan-spring-boot-test/src/main/java/cn/cocowwy/meituanspringboottest/Test.java
@@ -0,0 +1,22 @@
+package cn.cocowwy.meituanspringboottest;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Cocowwy
+ * @create 2022-04-04-20:52
+ */
+@Component
+public class Test implements ApplicationRunner {
+
+    /**
+     * 测试
+     * @param args
+     * @throws Exception
+     */
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
