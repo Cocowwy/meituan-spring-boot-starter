c455ee51d9c37209be2938612c78da03479c44a9
Cocowwy <514658459@qq.com>
2022-04-15 14:15:02 +0800

    push 1.0.0 release

--- meituan-spring-boot-test/src/main/java/cn/cocowwy/meituanspringboottest/Test.java
+++ meituan-spring-boot-test/src/main/java/cn/cocowwy/meituanspringboottest/Test.java
@@ -1,5 +1,6 @@
 package cn.cocowwy.meituanspringboottest;
 
+import cn.cocowwy.cocowwymeituan.api.MTMerchantApi;
 import cn.cocowwy.cocowwymeituan.api.MTOrderApi;
 import cn.cocowwy.meituancore.rs.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ import org.springframework.stereotype.Component;
 public class Test implements ApplicationRunner {
     @Autowired
     private MTOrderApi mtOrderApi;
+    @Autowired
+    private MTMerchantApi mtMerchantApi;
+
 
     /**
      * 测试
@@ -22,7 +26,13 @@ public class Test implements ApplicationRunner {
      * @throws Exception
      */
     public void run(ApplicationArguments args) {
-        Result result = mtOrderApi.orderDetail("123123");
-        System.out.println(result);
+        // 获取订单明细
+        Result result1 = mtOrderApi.orderDetail("123123");
+
+        // 获取商户状态
+        Result result2 = mtMerchantApi.getMerchantStatus("111");
+
+        // 商家接收订单
+        Result result3 = mtOrderApi.confirmOrder("123123");
     }
 }
